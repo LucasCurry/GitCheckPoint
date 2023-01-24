d40c6409ca824f6770f14b4adf574a9c61a83f15
LucasCurry <lucas.curry@brightstraining.com>
2023-01-24 13:24:27 +0100

    This is the second version of my program

--- src/CheckTest.java
+++ src/CheckTest.java
@@ -1,5 +1,27 @@
 public class CheckTest {
-    public static void main(String[] args) {
-        System.out.println("This will have to do.");
+    public CheckTest(int x, String futureProblem) {
+        this.x = x;
+        this.futureProblem = futureProblem;
     }
+
+    int x;
+    String futureProblem;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public String getFutureProblem() {
+        return futureProblem;
+    }
+
+    public void setFutureProblem(String futureProblem) {
+        this.futureProblem = futureProblem;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        CheckTest test = new CheckTest(2, "FutureProblem");
         System.out.println("This is the Git checkpoint task 1.1");
+        System.out.println(test.getFutureProblem());
+
+
+
     }
 }
