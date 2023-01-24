8e5073a5d92afbe457d8a731697c80c8d0522e6f
LucasCurry <lucas.curry@brightstraining.com>
2023-01-24 13:28:40 +0100

    This is the 1 version the branch Dev

---
+++ src/Calculate.java
@@ -0,0 +1,15 @@
+public class Calculate {
+
+    int x;
+    public Calculate(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
